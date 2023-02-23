2950e31b067e563546f97a1044c854b526e559f3
Pratik <pratiktiwari3792@gmail.com>
2023-02-24 01:08:33 +0530

    Learning decorators in TypeScript

--- src/main.ts
+++ src/main.ts
@@ -400,3 +400,124 @@ function createCourseGoal(
 }
 
 const names: Readonly<string[]> = ["mohit", "pratik"];
+
+// decorators
+
+// function Logger(constructor: Function) {
+//   console.log("Logging...");
+//   console.log(constructor);
+// }
+
+// using decorator factory design
+
+function Logger(logString: string) {
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function WithTemplate(template: string, hookId: string) {
+  console.log("Template Factory");
+  return function <T extends { new(...args: any[]): { name: string } }>(
+    OriginalConstructor: T
+  ) {
+    return class extends OriginalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h2")!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
+
+// @Logger
+@Logger("Logging - Human Being")
+@WithTemplate("<h2>My Human Being Object</h2>", "temp")
+class HumanBeing {
+  name = "Pratik";
+
+  constructor() {
+    console.log("creating a human being object...");
+  }
+}
+
+const newMan = new HumanBeing();
+console.log(newMan);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property Decorator");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator");
+  console.log(target, name, descriptor);
+}
+
+function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Method decorator");
+  console.log(target, name, descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter Decorator");
+  console.log(target, name, position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+function AutoBind(target: any, name: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const btn = document.querySelector("button");
+btn?.addEventListener("click", p.showMessage);
