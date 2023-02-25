6a5aeca1f74a0c3c4d4c0cbba3007dfd05627c76
pratik <pratiktiwari3792@gmail.com>
2023-02-25 15:47:12 +0530

    Validation using decorators

--- src/main.ts
+++ src/main.ts
@@ -380,19 +380,24 @@ console.log(textStorage.getItem());
 textStorage.removeItem("Pratik");
 console.log(textStorage.getItem());
 
-interface CourseGoal {
+interface Course {
   title: string;
+  price: number;
+}
+interface CourseGoal extends Course {
   description: string;
   completeUntil: Date;
 }
 
 function createCourseGoal(
   title: string,
+  price: number,
   description: string,
   date: Date
 ): CourseGoal {
   let courseGoal: Partial<CourseGoal> = {};
   courseGoal.title = title;
+  courseGoal.price = price;
   courseGoal.description = description;
   courseGoal.completeUntil = date;
 
@@ -521,3 +526,73 @@ const p = new Printer();
 
 const btn = document.querySelector("button");
 btn?.addEventListener("click", p.showMessage);
+
+const courseForm = document.querySelector("form")!;
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatable: string]: string[]; // ['required','positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propertyName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propertyName]: ["required"],
+  };
+}
+function PositiveNumber(target: any, propertyName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propertyName]: ["positive"],
+  };
+}
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+class NewCourse<T extends Course> {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(data: T) {
+    this.title = data.title;
+    this.price = data.price;
+  }
+}
+
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new NewCourse({ title, price });
+  if (!validate(createdCourse)) {
+    alert("Invalid Input, Please try again");
+    return;
+  }
+  console.log(createdCourse);
+});
